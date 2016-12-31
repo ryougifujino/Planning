017737bedd00c9b5aaa18ecf655c2a3902970263
ryougifujino <ryougifujino@gmail.com>
2017-01-01 02:40:02 +0800

    upgrade:升级baseF/A 里的AlertDialog为v7

--- lib/src/main/java/com/yurikami/lib/base/BaseActivity.java
+++ lib/src/main/java/com/yurikami/lib/base/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.yurikami.lib.base;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
@@ -8,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
--- lib/src/main/java/com/yurikami/lib/base/BaseFragment.java
+++ lib/src/main/java/com/yurikami/lib/base/BaseFragment.java
@@ -1,12 +1,12 @@
 package com.yurikami.lib.base;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
